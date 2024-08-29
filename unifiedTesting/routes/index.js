cb40258a0ccdb869ede278ca6afad3bd0a954e77
Somanshu <somanshu63@gmail.com>
2024-08-29 11:43:40 +0530

    post route

--- unifiedTesting/routes/index.js
+++ unifiedTesting/routes/index.js
@@ -6,10 +6,15 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 router.get("/success", function (req, res, next) {
+  console.log(req.body);
   res.render("index", { title: "Success" });
 });
 router.get("/failure", function (req, res, next) {
   res.render("index", { title: "Failure" });
+});
+router.post("/success", function (req, res, next) {
+  console.log(req.body);
+  res.render("index", { title: "post success" });
 });
 
 module.exports = router;
